ce3c66e75796bdd0544c56fc2f41f67d4a0dcc36
mohit-dhote <mohitdhote465@gmail.com>
2024-11-16 21:49:04 +0530

    Initial commit

---
+++ TryCatA.java
@@ -0,0 +1,15 @@
+public class TryCatA {
+
+  public static void main(String[] args){
+    int a = 10, b =0, c;
+    try{
+        c = a/b;
+        System.out.println(c);
+    }
+    catch(ArithmeticException e){
+        System.out.println(e);
+    }
+    System.out.println("Program ended successfully");
+  }
+    
+}
---
+++ exceptionA.java
@@ -0,0 +1,9 @@
+public class exceptionA {
+    public static void main(String[] args){
+        int a =10, b=0, c;
+        c = a/b;
+        System.out.println(c);
+        System.out.println("Hello");
+    }
+    
+}
